70a1def1f7b2764b8a10375b4d3ec963afbc4b50
German Abramov <germanjke@gmail.com>
2023-08-20 23:39:26 +0300

    init food commit

---
+++ Diffusion-macOS/GeneratedImageView.swift
@@ -0,0 +1,57 @@
+//
+//  GeneratedImageView.swift
+//  Diffusion
+//
+//  Created by German Abramov on 20/08/23.
+
+import SwiftUI
+
+struct GeneratedImageView: View {
+    @EnvironmentObject var generation: GenerationContext
+    
+    var body: some View {
+        switch generation.state {
+        case .startup: return AnyView(Image("placeholder").resizable())
+        case .running(let progress):
+            guard let progress = progress, progress.stepCount > 0 else {
+                // The first time it takes a little bit before generation starts
+                return AnyView(ProgressView())
+            }
+            let step = Int(progress.step) + 1
+            let fraction = Double(step) / Double(progress.stepCount)
+            let label = "Step \(step) of \(progress.stepCount)"
+            return AnyView(HStack {
+                ProgressView(label, value: fraction, total: 1).padding()
+                Button {
+                    generation.cancelGeneration()
+                } label: {
+                    Image(systemName: "x.circle.fill").foregroundColor(.gray)
+                }
+                .buttonStyle(.plain)
+            })
+        case .complete(_, let image, _, _):
+            guard let theImage = image else {
+                return AnyView(Image(systemName: "exclamationmark.triangle").resizable())
+            }
+                              
+            return AnyView(
+                    Image(theImage, scale: 1, label: Text("generated"))
+                    .resizable()
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .contextMenu {
+                        Button {
+                            NSPasteboard.general.clearContents()
+                            let nsimage = NSImage(cgImage: theImage, size: NSSize(width: theImage.width, height: theImage.height))
+                            NSPasteboard.general.writeObjects([nsimage])
+                        } label: {
+                            Text("Copy Photo")
+                        }
+                    }
+            )
+        case .failed(_):
+            return AnyView(Image(systemName: "exclamationmark.triangle").resizable())
+        case .userCanceled:
+            return AnyView(Text("Generation canceled"))
+        }
+    }
+}
---
+++ Diffusion/Common/Pipeline/Pipeline.swift
@@ -0,0 +1,90 @@
+//
+//  Pipeline.swift
+//  Diffusion
+//
+//  Created by Pedro Cuenca on December 2022.
+//  See LICENSE at https://github.com/huggingface/swift-coreml-diffusers/LICENSE
+//
+
+import Foundation
+import CoreML
+import Combine
+
+import StableDiffusion
+
+typealias StableDiffusionProgress = StableDiffusionPipeline.Progress
+
+struct GenerationResult {
+    var image: CGImage?
+    var lastSeed: UInt32
+    var interval: TimeInterval?
+    var userCanceled: Bool
+    var itsPerSecond: Double?
+}
+
+class Pipeline {
+    let pipeline: StableDiffusionPipeline
+    let maxSeed: UInt32
+    
+    var progress: StableDiffusionProgress? = nil {
+        didSet {
+            progressPublisher.value = progress
+        }
+    }
+    lazy private(set) var progressPublisher: CurrentValueSubject<StableDiffusionProgress?, Never> = CurrentValueSubject(progress)
+    
+    private var canceled = false
+
+    init(_ pipeline: StableDiffusionPipeline, maxSeed: UInt32 = UInt32.max) {
+        self.pipeline = pipeline
+        self.maxSeed = maxSeed
+    }
+    
+    func generate(
+        prompt: String,
+        negativePrompt: String = "",
+        scheduler: StableDiffusionScheduler,
+        numInferenceSteps stepCount: Int = 50,
+        seed: UInt32? = nil,
+        guidanceScale: Float = 7.5,
+        disableSafety: Bool = false
+    ) throws -> GenerationResult {
+        let beginDate = Date()
+        canceled = false
+
+        let theSeed = seed ?? UInt32.random(in: 0...maxSeed)
+        let sampleTimer = SampleTimer()
+        sampleTimer.start()
+        
+        var config = StableDiffusionPipeline.Configuration(prompt: prompt)
+        config.negativePrompt = negativePrompt
+        config.stepCount = stepCount
+        config.seed = theSeed
+        config.guidanceScale = guidanceScale
+        config.disableSafety = disableSafety
+        config.schedulerType = scheduler
+        
+        let images = try pipeline.generateImages(configuration: config) { progress in
+            sampleTimer.stop()
+            handleProgress(progress, sampleTimer: sampleTimer)
+            if progress.stepCount != progress.step {
+                sampleTimer.start()
+            }
+            return !canceled
+        }
+        let interval = Date().timeIntervalSince(beginDate)
+        print("Got images: \(images) in \(interval)")
+        
+        // Unwrap the 1 image we asked for, nil means safety checker triggered
+        let image = images.compactMap({ $0 }).first
+        return GenerationResult(image: image, lastSeed: theSeed, interval: interval, userCanceled: canceled, itsPerSecond: 1.0/sampleTimer.median)
+    }
+
+    func handleProgress(_ progress: StableDiffusionPipeline.Progress, sampleTimer: SampleTimer) {
+        self.progress = progress
+    }
+        
+    func setCancelled() {
+        canceled = true
+    }
+}
